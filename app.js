b1d4627ac3c8c81637c15e792f3f32bcfc2c6412
renyuankun <ryk@jumifinance.com>
2018-04-11 21:01:50 +0800

    init koa2

---
+++ app.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(async(ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+
+    ctx.response.body = '<h2>Hello Koa2</h2>';
+});
+
+
+app.listen(3000);
+console.log('app started at 3000端口...');
