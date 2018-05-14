83c1244f6ac5aeba1487fb6686446aa85fe0b467
renyuankun <ryk@jumifinance.com>
2018-05-14 18:45:46 +0800

    修改file_server

--- file_server.js
+++ file_server.js
@@ -1,27 +1,33 @@
-'use starct'
+"use starct";
 
-var fs = require('fs'),
-    url = require('url'),
-    path = require('path'),
-    http = require('http');
+var fs = require("fs"),
+    url = require("url"),
+    path = require("path"),
+    http = require("http");
 
 // 从命令行参数获取root目录，默认是当前目录；
-var root = path.resolve(process.argv[2] || '.');
+var root = path.resolve(process.argv[2] || ".");
 
-console.log('Static root dir:' + root);
+console.log("Static root dir:" + root);
 
 // 创建服务器；
-var server = http.createServer(function (require, response) {
+var server = http.createServer(function (request, response) {
+    // console.log('--------------回调函数的参数');
+    // console.log(request);
+    // console.log(response);
     // 获取URL的path, 类似'css/bootstrap.css',
-    var pathname = url.parse(require.url).pathname;
-    // 获取对应的贝蒂文件路径，类似 '/srv/www/css/bootstrap.css' 
+    var pathname = url.parse(request.url).pathname;
+    // 获取对应的贝蒂文件路径，类似 '/srv/www/css/bootstrap.css'
     var filepath = path.join(root, pathname);
 
     // 获取文件状态
     fs.stat(filepath, function (err, stats) {
+        console.log('--------------回调函数的参数');
+        console.log(err);
+        console.log(stats);
         if (!err && stats.isFile()) {
             // 没有出错并且文件存在；
-            console.log('200' + require.url);
+            console.log("200" + request.url);
             // 发送200响应；
             response.writeHead(200);
 
@@ -29,13 +35,13 @@ var server = http.createServer(function (require, response) {
             fs.createReadStream(filepath).pipe(response);
         } else {
             // 出错了或者文件不存在；
-            console.log('404' + request.url);
+            console.log("404" + request.url);
             // 发送404响应；
             response.writeHead(404);
-            response.end('404 Not Fund');
+            response.end("404 Not Fund");
         }
     });
 });
 
 server.listen(9200);
-console.log('server is running at http://127.0.0.1:9200/');
+console.log("server is running at http://127.0.0.1:9200/");
--- hellohttp.js
+++ hellohttp.js
@@ -1,15 +1,25 @@
 "use stract";
 
+// 导入http模块
 var http = require("http");
 
-var server = http.createServer(function(require, response) {
-  // console.log(require);
+// 创建http server,并传入回调函数
+var server = http.createServer(function (request, response) {
+  // console.log(request);
   // console.log(response);
-  console.log(require.method + ":" + require.url);
-  response.writeHead(200, { "Content-Type": "text/html" });
+  // 回调函数接收request 和 response对象
+  // 获取http请求的url和method
+  console.log(request.method + ":" + request.url);
+
+  // 将http响应200写入response, 同时设置  Content-Type: text/html;
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  // 将http相应的html内容写入response;
   response.end("<h1>hello World</h1>");
 });
 
+// 让服务器监听端口；
 server.listen(9005);
 
 console.log("Server is running at http://127.0.0.1:9005/");
